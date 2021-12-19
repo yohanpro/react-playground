5f92ab8694bcf5d9402dfe1ea99bc29451aab34e
Yohan <yohan@yohanpro.com>
2021-12-19 14:24:25 +0900

    feat: emotion styled 설치

--- packages/notion-clone/src/components/trap.tsx
+++ packages/notion-clone/src/components/trap.tsx
@@ -1,11 +1,18 @@
 import * as React from "react";
+import styled from "@emotion/styled";
 
 const Trap = () => {
   return (
-    <div>
+    <TrapContainer>
       <h1>Trap</h1>
-    </div>
+    </TrapContainer>
   );
 };
+
+const TrapContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #f0f0f0;
+`;
 
 export default Trap;
