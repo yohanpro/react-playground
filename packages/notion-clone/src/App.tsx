eb3ecb2261c5d182914dd8d532da1a30fcd15959
Yohan <yohan@yohanpro.com>
2021-12-19 13:07:07 +0900

    feat: monorepo tutorial for typescript react

---
+++ packages/notion-clone/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { HelloWorld } from "my-module";
+
+const App = () => {
+  return (
+    <div>
+      App을 시작해볼까?
+      <HelloWorld />
+    </div>
+  );
+};
+
+export default App;
