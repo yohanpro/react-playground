df132c5c00589a0274582700b330f240bfe0f93b
Yohan <yohan@yohanpro.com>
2022-04-02 20:00:15 +0900

    feat: todo list basic setup

---
+++ packages/todo/components/home/index.tsx
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+const Home = () => {
+  return <HomeContainer>This is Home</HomeContainer>;
+};
+
+const HomeContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #f5f5f5;
+`;
+export default Home;
