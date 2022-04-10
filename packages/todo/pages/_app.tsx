8929743a30e43ace378d945678e54997ffec6feb
Yohan <yohan@yohanpro.com>
2022-04-10 11:50:14 +0900

    feat: 필요없는 파일 제거

--- packages/todo/pages/_app.tsx
+++ packages/todo/pages/_app.tsx
@@ -3,6 +3,10 @@ import "../styles/global.scss";
 import type { ReactElement, ReactNode } from "react";
 import type { NextPage } from "next";
 
+type GetLayout = (page: ReactNode) => ReactNode;
+
+const defaultGetLayout: GetLayout = (page: ReactNode): ReactNode => page;
+
 type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
 };
@@ -13,7 +17,8 @@ type AppPropsWithLayout = AppProps & {
 
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   // Use the layout defined at the page level, if available
-  const getLayout = Component.getLayout ?? ((page) => page);
+
+  const getLayout = Component.getLayout ?? defaultGetLayout;
 
   return getLayout(<Component {...pageProps} />);
 }
